d680c4d9d36e9965230ccd80b1b03ff3b4daf9dc
bayswiss <antonio.bs@undabit.com>
2026-02-27 13:37:30 +0100

    Added copyright header + dokken

--- demos/demo_helmholtz.py
+++ demos/demo_helmholtz.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2026 Antonio Baiano Svizzero
+#
+# This file is part of FEniCSx_PML (https://github.com/bayswiss/fenicsx_pml)
+#
+# SPDX-License-Identifier:    GPL-3.0-or-later
+
 import numpy as np
 import ufl
 from dolfinx import la
